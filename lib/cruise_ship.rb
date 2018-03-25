fded3c3cee118d7feeed4d62136227ef91bf55ec
Vanessa Toro <vanetoro84@gmail.com>
2018-03-25 00:35:20 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,9 +7,9 @@
 # }
 
 def select_winner(passengers)
-  # winner = ''
-  # passengers.each do |suite, name|
-  #   if suite == :suite_a && name.starts_with?("A")
-  #     winner = name
-  #   end
+  winner = ''
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.starts_with?("A")
+      winner = name
+    end
 end
